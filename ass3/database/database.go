74247cf7335d863b709b53039fc6eae39dceefb4
Zarina <another.zarina@gmail.com>
2023-04-10 11:34:36 +0600

    wtf

--- ass3/database/database.go
+++ ass3/database/database.go
@@ -6,8 +6,7 @@ import (
 )
 
 func GETDB() (*gorm.DB, error) {
-	dsn := "postgres://ZarinaKab:DbxWZqF1UI0L@ep-wandering-haze-525394.ap-southeast-1.aws.neon.tech/neondb"
-
+	dsn := "url"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("I can't connect with database")
